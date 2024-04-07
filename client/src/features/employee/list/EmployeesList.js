abe9ddfc2e7fb982c87bd3bb634e7e0a91391ba9
Mrs-ID <i6725155@gmail.com>
2024-04-07 08:57:37 +0300

    upload project

---
+++ client/src/features/employee/list/EmployeesList.js
@@ -0,0 +1,61 @@
+import { Link } from "react-router-dom"
+import { useDeleteEmployeeMutation, useGetAllEmployeesQuery } from "../employeesApiSlice"
+import "./EmployeesList.css"
+import Search from "../../../components/search/Search"
+const EmployeesList = () => {
+    const { data: employeesObj, isError, error, isLoading,isSuccess } = useGetAllEmployeesQuery()
+    const [deleteEmployee,{isSuccess:isDeleteSuccess}]=useDeleteEmployeeMutation()
+    const deleteClick =(employee)=>{
+        if(window.confirm("האם אתה בטוח שברצונך למחוק את העובד"))
+        {
+            deleteEmployee({_id:employee._id})
+        }
+    }
+    if (isLoading)
+        return <h1>Loading...</h1>
+    if (isError)
+        return <h1>{JSON.stringify(error)}helllo</h1>
+    return (
+        <div className="employees-list">
+            <div className="employees-list-top">
+                <Search placeholder="חיפוש לפי שם נציג" />
+                <Link to="/dash/employees/add" className="employees-list-add-button">הוספת נציג</Link>
+            </div>
+            <table className="employees-list-table">
+                <thead>
+                    <tr>
+                        <td>שם העובד</td>
+                        <td>פלאפון</td>
+                        <td>אימייל</td>
+                        <td>תפקיד</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employeesObj.data?.map(employee =>
+                        <tr key={employee._id}>
+                            <td>
+                                <div className="employees-list-employee">
+                                    {employee.name}
+                                </div>
+                            </td>
+                            <td>
+                                {employee.phone}
+                            </td>
+                            <td>
+                                {employee.email}
+                            </td>
+                            <td>
+                                {employee.role}
+                            </td>
+                            <td>
+                                <Link to={`/dash/employees/${employee._id}`} className="employees-list-button employees-list-view">עדכון</Link>
+                                <button onClick={()=>{deleteClick(employee)}} className="employees-list-button employees-list-delete">מחיקה</button>
+                            </td>
+                        </tr>)}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default EmployeesList
---
+++ client/src/features/family/familiesApiSlice.js
@@ -0,0 +1,36 @@
+import apiSlice from "../../app/apiSlice";
+
+const familiesApiSlice = apiSlice.injectEndpoints({
+    endpoints: (build) => ({
+        getAllFamilies: build.query({
+            query: () => ({
+                url: "/api/family"
+            })
+        }),
+        addFamily:build.mutation({
+            query:(family)=>({
+                url:"/api/family",
+                method:"POST",
+                body:family
+            })
+        }),
+        updateFamily: build.mutation({
+            query: (family) => ({
+                url: "/api/family",
+                method: "PUT",
+                body: family
+            }),
+            invalidatesTags: ["Families"]
+        }),
+        deleteFamily: build.mutation({
+            query: ({_id}) => ({
+                url: "/api/family",
+                method: "DELETE",
+                body: {_id}
+            }),
+            invalidatesTags: ["Families"]
+        })
+    })
+    
+})
+export const { useGetAllFamiliesQuery,useAddFamilyMutation,useUpdateFamilyMutation,useDeleteFamilyMutation } = familiesApiSlice
