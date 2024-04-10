097e81680ecb7e9f6688abd508cee26310574b23
Mrs-ID <i6725155@gmail.com>
2024-04-10 14:59:00 +0300

    install cookie-parser

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 require("dotenv").config()
 const express=require("express")
 const cors=require("cors")
-const coo=require("cors")
+const cookieParser=require("cookie-parser")
 const corsOptions=require("./config/corsOptions")
 const app=express()
 const PORT=process.env.PORT||1234
 const connectDB=require("./config/connectDB")
 app.use(cors(corsOptions))
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.static("public"))
+
 connectDB()
 
 app.use("/api/family", require("./routes/FamilyRoute"))
