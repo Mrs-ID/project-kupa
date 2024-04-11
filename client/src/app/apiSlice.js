a01639cefc93144c738d8c0073c3bac5d2f3f6e3
Mrs-ID <i6725155@gmail.com>
2024-04-11 18:14:02 +0300

    update apiSlice - add token to all queries from client

--- client/src/app/apiSlice.js
+++ client/src/app/apiSlice.js
@@ -1,9 +1,20 @@
 import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+
+const baseQuery=fetchBaseQuery({
+    baseUrl:"http://localhost:1111",
+    credentials:'include',
+    prepareHeaders:(headers,{getState})=>{
+        const token=getState().auth.token
+        if(token){
+            headers.set("authorization", `Bearer ${token}`)
+        }
+        return headers
+    }
+})
+
 const apiSlice=createApi({
     reducerPath:"api",
-    baseQuery:fetchBaseQuery({
-        baseUrl:"http://localhost:1111"
-    }),
+    baseQuery,
     endpoints:()=>({})
 })
 export default apiSlice
