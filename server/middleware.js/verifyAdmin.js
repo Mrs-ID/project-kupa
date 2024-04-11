b4846f4c1e084e2172a4dbc1767ced0d3f12f33e
Mrs-ID <i6725155@gmail.com>
2024-04-11 17:46:02 +0300

    Add the middleware verifyAdmin

---
+++ server/middleware.js/verifyAdmin.js
@@ -0,0 +1,12 @@
+const verifyAdmin=(req,res,next)=>{
+    if(req.user&&req.user.role==="מנהל"){
+        next()
+    }
+    else{
+        return res.status(401).json({
+            error: true,
+            message: "Unauthorized Admin",
+            data: null
+        })
+    }
+}
